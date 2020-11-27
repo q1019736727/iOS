42f90ba8d274f6a61bf2a4ad33189b09d6c3c6d6
q1019736727 <1837060380@qq.com>
2020-11-27 09:41:06 +0800

    线程

--- 27 gcd/27 gcd/ViewController.m
+++ 27 gcd/27 gcd/ViewController.m
@@ -19,16 +19,67 @@
     NSLog(@"任务1");
     
     dispatch_queue_t queue = dispatch_get_main_queue();
-    dispatch_async(queue, ^{
+    dispatch_sync(queue, ^{
         NSLog(@"任务2");
     });
     
     NSLog(@"任务3");
 }
 
+- (void)test2{
+    
+    NSLog(@"任务1");
+    //创建串行队列
+    dispatch_queue_t queue = dispatch_queue_create("myQueue", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_async(queue, ^{ //block0
+        
+        NSLog(@"任务2");
+        
+        dispatch_sync(queue, ^{//block1
+            NSLog(@"任务3");
+        });
+        
+        NSLog(@"任务4");
+    });
+    
+}
+- (void)test3{
+    NSLog(@"任务1");
+    //创建串行队列
+    dispatch_queue_t queue = dispatch_queue_create("myQueue", DISPATCH_QUEUE_SERIAL);
+    dispatch_queue_t queue2 = dispatch_queue_create("myQueue2", DISPATCH_QUEUE_SERIAL);
+    dispatch_async(queue, ^{ //block0
+        
+        NSLog(@"任务2");
+        
+        dispatch_sync(queue2, ^{//block1
+            NSLog(@"任务3");
+        });
+        
+        NSLog(@"任务4");
+    });
+}
+- (void)test4{
+    NSLog(@"任务1");
+    //创建并发队列
+    dispatch_queue_t queue = dispatch_queue_create("queue", DISPATCH_QUEUE_CONCURRENT);
+    dispatch_async(queue, ^{
+        
+        NSLog(@"任务2");
+
+        dispatch_sync(queue, ^{
+            NSLog(@"任务3");
+        });
+        
+        NSLog(@"任务4");
+
+    });
+    NSLog(@"任务5");
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self test4];
 }
 
 
